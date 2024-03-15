6bfbe3a25c036f8767e87d2616478a5fcee3ea5f
Braulio Camarena <braulio.camarenah@gmail.com>
2024-03-15 17:16:05 -0600

    fix incline file name

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -57,8 +57,8 @@ return {
           return {
             { get_diagnostic_label() },
             { get_git_diff() },
-            { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
-            { filename .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
+            { guibg = "none" },
+            { "" .. " ", gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
             { "┊  " .. vim.api.nvim_win_get_number(props.win), group = "DevIconWindows" },
           }
         end,
