7db88307dfdfa9c066cc488551dd1b62aa0ea301
Braulio Camarena <braulio.camarenah@gmail.com>
2024-03-25 18:05:51 -0600

    add vim-visual-multi

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,4 +1,5 @@
 return {
+  { "mg979/vim-visual-multi", branch = "master" },
   { "xiyaowong/transparent.nvim" },
   {
     enabled = false,
@@ -13,27 +14,8 @@ return {
     },
   },
   {
-    "echasnovski/mini.hipatterns",
-    event = "BufReadPre",
-    opts = {
-      highlighters = {
-        hsl_color = {
-          pattern = "hsl%(%d+,? %d+%%?,? %d+%%?%)",
-          group = function(_, match)
-            local utils = require("solarized-osaka.hsl")
-            --- @type string, string, string
-            local nh, ns, nl = match:match("hsl%((%d+),? (%d+)%%?,? (%d+)%%?%)")
-            --- @type number?, number?, number?
-            local h, s, l = tonumber(nh), tonumber(ns), tonumber(nl)
-            --- @type string
-            local hex_color = utils.hslToHex(h, s, l)
-            return MiniHipatterns.compute_hex_color_group(hex_color, "bg")
-          end,
-        },
-      },
-    },
+    "NvChad/nvim-colorizer.lua",
   },
-
   {
     "dinhhuy258/git.nvim",
     event = "BufReadPre",
@@ -176,12 +158,12 @@ return {
                 vim.cmd("startinsert")
               end,
               ["<C-u>"] = function(prompt_bufnr)
-                for i = 1, 10 do
+                for _ = 1, 10 do
                   actions.move_selection_previous(prompt_bufnr)
                 end
               end,
               ["<C-d>"] = function(prompt_bufnr)
-                for i = 1, 10 do
+                for _ = 1, 10 do
                   actions.move_selection_next(prompt_bufnr)
                 end
               end,
