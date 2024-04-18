9babaee844d78599f9c3cb77db65e504c3404f49
antiox sistemas <sistemas@antioxvode.com>
2024-04-18 17:32:08 -0600

    add rainbow-delimiters

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,6 +1,7 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = { "hiphish/rainbow-delimiters.nvim" },
     opts = {
       ensure_installed = {
         "astro",
@@ -13,9 +14,14 @@ return {
         "graphql",
         "http",
         "java",
+        "html",
+        "typescript",
+        "tsx",
         "php",
         "rust",
         "scss",
+        "ruby",
+        "lua",
         "sql",
         "svelte",
       },
