30a149d4fffcd619ae2f67b5c367c897d8a46fb9
Braulio Camarena <braulio.camarenah@gmail.com>
2024-06-08 12:14:00 -0600

    use lua plugin instead

--- lua/plugins/cutlass.lua
+++ lua/plugins/cutlass.lua
@@ -1,3 +1,8 @@
 return {
-  { "svermeulen/vim-cutlass" },
+  "gbprod/cutlass.nvim",
+  opts = {
+    -- your configuration comes here
+    -- or don't set opts to use the default settings
+    -- refer to the configuration section below
+  },
 }
