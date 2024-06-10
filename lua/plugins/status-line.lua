5f0c8e8b5c8033d5e811c24b81ca7fa5755a9833
BrauCamaH <braulio.camarenah@gmail.com>
2024-06-09 22:46:02 -0600

    change status-line

--- lua/plugins/status-line.lua
+++ lua/plugins/status-line.lua
@@ -34,18 +34,14 @@ return {
       options = {
         theme = bubbles_theme,
         component_separators = "",
-        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
         lualine_b = { "branch" },
         lualine_c = {
           "%=", --[[ add your center compoentnts here in place of this comment ]]
         },
         lualine_y = { "filetype", "progress" },
-        lualine_z = {
-          { "location", separator = { right = "" }, left_padding = 2 },
-        },
+        lualine_z = {},
       },
     },
   },
