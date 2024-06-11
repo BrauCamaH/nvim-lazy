bd8a1687ec27aa2a5a0c8cb0d4c33f865b7efa8f
antiox sistemas <sistemas@antioxvode.com>
2024-06-11 11:12:12 -0600

    change status line

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -25,23 +25,7 @@ return {
           end
           local ft_icon, ft_color = devicons.get_icon_color(filename)
 
-          local function get_diagnostic_label()
-            local icons = { error = " ", warn = " ", info = " ", hint = "✨" }
-            local label = {}
-
-            for severity, icon in pairs(icons) do
-              local n = #vim.diagnostic.get(props.buf, { severity = vim.diagnostic.severity[string.upper(severity)] })
-              if n > 0 then
-                table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
-              end
-            end
-            if #label > 0 then
-              table.insert(label, { "┊ " })
-            end
-            return label
-          end
           return {
-            { get_diagnostic_label() },
             { (ft_icon or "") .. " ", guifg = ft_color, guibg = "none" },
             { filename .. " ", guifg = colors.white, gui = vim.bo[props.buf].modified and "bold,italic" or "bold" },
           }
