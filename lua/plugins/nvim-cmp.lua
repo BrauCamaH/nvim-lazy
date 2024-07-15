35615b8c7766cc2929d8bcfdb17287a42b4e368b
Braulio Camarena <braulio.camarenah@gmail.com>
2024-07-15 17:33:23 -0600

    fix cmp adn lsp errors

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -52,7 +52,6 @@ return {
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = "menu,menuone,noinsert" },
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
         --
