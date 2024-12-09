d4185a0a291194b9d8746f8ac5e4277ef523697c
Braulio Camarena <braulio.camarenah@gmail.com>
2024-12-09 13:46:30 -0600

    include oil.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -114,8 +114,6 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     dependencies = {
-      "nvim-telescope/telescope-file-browser.nvim",
-      "nvim-telescope/telescope-media-files.nvim",
       "nvim-lua/popup.nvim",
       "nvim-lua/plenary.nvim",
     },
@@ -128,47 +126,9 @@ return {
         end,
         desc = "Lists Function names, variables, from Treesitter",
       },
-      {
-        ",,",
-        function()
-          local telescope = require("telescope")
-
-          local function telescope_buffer_dir()
-            return vim.fn.expand("%:p:h")
-          end
-
-          telescope.extensions.file_browser.file_browser({
-            path = "%:p:h",
-            cwd = telescope_buffer_dir(),
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            previewer = false,
-            initial_mode = "normal",
-            layout_strategy = "vertical",
-            layout_config = { height = 40 },
-          })
-        end,
-        desc = "Open File Browser with the path of the current buffer",
-      },
-      {
-        "<leader>slm",
-        function()
-          require("telescope").extensions.media_files.media_files({
-            path = "%:p:h",
-            respect_gitignore = false,
-            hidden = true,
-            grouped = true,
-            initial_mode = "insert",
-          })
-        end,
-        desc = "Show media files",
-      },
     },
     config = function(_, opts)
       local telescope = require("telescope")
-      local actions = require("telescope.actions")
-      local fb_actions = require("telescope").extensions.file_browser.actions
 
       opts.defaults = vim.tbl_deep_extend("force", opts.defaults, {
         wrap_results = true,
@@ -189,46 +149,7 @@ return {
           },
         },
       }
-      opts.extensions = {
-        media_files = {
-          -- filetypes whitelist
-          -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
-          filetypes = { "png", "webp", "jpg", "jpeg", "svg" },
-          -- find command (defaults to `fd`)
-          find_cmd = "rg",
-        },
-        file_browser = {
-          theme = "dropdown",
-          -- disables netrw and use telescope-file-browser in its place
-          hijack_netrw = true,
-          mappings = {
-            -- your custom insert mode mappings
-            ["n"] = {
-              -- your custom normal mode mappings
-              ["n"] = fb_actions.create,
-              ["h"] = fb_actions.goto_parent_dir,
-              ["/"] = function()
-                vim.cmd("startinsert")
-              end,
-              ["<C-u>"] = function(prompt_bufnr)
-                for _ = 1, 10 do
-                  actions.move_selection_previous(prompt_bufnr)
-                end
-              end,
-              ["<C-d>"] = function(prompt_bufnr)
-                for _ = 1, 10 do
-                  actions.move_selection_next(prompt_bufnr)
-                end
-              end,
-              ["<PageUp>"] = actions.preview_scrolling_up,
-              ["<PageDown>"] = actions.preview_scrolling_down,
-            },
-          },
-        },
-      }
       telescope.setup(opts)
-      require("telescope").load_extension("file_browser")
-      require("telescope").load_extension("media_files")
     end,
   },
   -- Incremental rename
