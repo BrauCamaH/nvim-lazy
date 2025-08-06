0bce536b02a1854d24d1c6104399e0535e074f07
Antiox Sistemas <sistemas.antioxvode@gmail.com>
2025-08-06 13:43:43 -0600

    fix zen mode

--- lua/plugins/zen-mode.lua
+++ lua/plugins/zen-mode.lua
@@ -1,16 +1,28 @@
 return {
-  -- Lua
-  {
-    "preservim/vim-pencil",
-  },
   {
     "folke/twilight.nvim",
-    ft = "markdown",
     opts = {
-      -- your configuration comes here
-      -- or leave it empty to use the default settings
-      -- refer to the configuration section below
+      dimming = {
+        alpha = 0.6, -- lighter dimming
+        color = { "Normal", "#ffffff" },
+        inactive = true,
+      },
+      context = 10,
+      expand = {
+        "function",
+        "method",
+        "class",
+        "if_statement",
+        "for_statement",
+        "while_statement",
+        "switch_statement",
+        "table",
+      },
     },
+  },
+  {
+    "preservim/vim-pencil",
+    ft = { "markdown", "text" },
   },
   {
     "folke/zen-mode.nvim",
@@ -18,19 +30,20 @@ return {
       "folke/twilight.nvim",
       "preservim/vim-pencil",
     },
-    ft = { "markdown", "text" }, -- optional: limit to filetypes
+    ft = { "markdown", "text", "lua", "python", "typescript", "javascript" },
     config = function()
       require("zen-mode").setup({
         window = {
           backdrop = 0.95,
-          width = 120,
+          width = 80,
           height = 1,
           options = {
             signcolumn = "no",
             number = false,
             relativenumber = false,
-            wrap = true,
+            wrap = false,
             linebreak = true,
+            colorcolumn = "",
           },
         },
         plugins = {
@@ -40,7 +53,7 @@ return {
             showcmd = false,
             laststatus = 0,
           },
-          twilight = { enabled = true },
+          twilight = { enabled = false },
           gitsigns = { enabled = false },
           tmux = { enabled = true },
           wezterm = {
@@ -50,22 +63,31 @@ return {
         },
       })
 
-      -- Force wrap and linebreak after ZenMode enters
+      -- Force wrap settings and apply Twilight highlight on ZenMode enter
       vim.api.nvim_create_autocmd("User", {
         pattern = "ZenModeEnter",
         callback = function()
           vim.opt.wrap = true
           vim.opt.linebreak = true
+          vim.opt.textwidth = 80
+          vim.opt.colorcolumn = ""
+
+          -- Set Twilight highlight for Nightfly theme
+          vim.api.nvim_set_hl(0, "Twilight", {
+            fg = "#7f8c99", -- subtle cool gray-blue
+            blend = 20, -- slight transparency
+            italic = true,
+          })
         end,
       })
     end,
     keys = {
       {
         "<leader>z",
-        desc = "Toggle Zen Mode with Pencil",
+        desc = "Toggle Zen Mode with PencilSoft",
         function()
           vim.cmd("ZenMode")
-          vim.cmd("PencilSoft") -- soft wrap mode for writing
+          vim.cmd("PencilSoft")
         end,
       },
     },
