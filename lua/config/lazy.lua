f20ae90e7973e631f9abd318ce6e13a21d530480
braucamah <braulio.camarenah@gmail.com>
2025-11-04 20:22:50 -0600

    include golang and rust

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
