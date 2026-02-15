94f627396f8d5551b1e9a09602798ed4b5f13972
Braulio Camarena <braulio.camarenah@gmail.com>
2026-02-14 21:21:16 -0600

    include tysp

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -25,6 +25,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
     { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.typst" },
     { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.editor.snacks_picker" },
